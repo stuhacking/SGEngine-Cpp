11c202d98dff6febc9df4742c59d81d941715be4
Stuart Hacking <stuhacking@gmail.com>
2016-07-19 09:25:52 +0100

    Tidy and document CMake scripts

--- deps.sh
+++ deps.sh
@@ -1,8 +1,12 @@
 #! /bin/sh
-# Download 3rd party deps
+# Download 3rd party dependencies.
 
 # Google Test/Mock lib
-git clone https://github.com/google/googletest.git ./External/googletest
+if [ ! -d './External/googletest' ] ; then
+    git clone https://github.com/google/googletest.git ./External/googletest
+fi
 
 # JSON Parser
-#git clone https://github.com/miloyip/rapidjson.git ./External/rapidjson
+#if [ ! -d './External/rapidjson' ] ; then
+#    git clone https://github.com/miloyip/rapidjson.git ./External/rapidjson
+#fi

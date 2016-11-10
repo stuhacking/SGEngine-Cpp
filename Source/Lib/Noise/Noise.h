14c2b8cd69b3906cb1daa0a5e2e59d363bd92f66
Stuart Hacking <stuhacking@gmail.com>
2016-11-10 16:11:30 +0000

    Streamline Documentation generator
    + Update noise documentation

--- Source/Lib/Noise/Noise.h
+++ Source/Lib/Noise/Noise.h
@@ -18,16 +18,61 @@ namespace sge {
 
 class Noise {
 public:
+    /**
+     * Generate a pseudorandom scalar hash value for coordinate (x, y).
+     *
+     * @param x x coordinate.
+     * @param y y coordinate.
+     * @return Pseudorandom hash value.
+     */
     static i32 Hash (const i32 x, const i32 y = 0);
 
+    /**
+     * Generate a simple white noise value for coordinate (x, y).
+     *
+     * @param x x coordinate.
+     * @param y y coordinate.
+     * @return Simple noise value.
+     */
     static float SimpleNoise (const i32 x, const i32 y = 0);
 
+    /**
+     * Generate a smoothed noise value for coordinate (x, y).
+     *
+     * @param x x coordinate.
+     * @param y y coordinate.
+     * @return Smoothed noise value.
+     */
     static float SmoothNoise (const float x, const float y = 0);
 
+    /**
+     * Generate a Perlin like simplex noise value for coordinate (x, y).
+     *
+     * Implements a cloudy 2D noise pattern similar to Perlin. Changing the frequency
+     * and (x, y) offsets allows you to sample from different portions of the field
+     * to achieve random patterns.
+     *
+     * @param x x coordinate.
+     * @param y y coordinate.
+     * @param p_freq Frequency of pattern (Higher means smaller clouds).
+     * @param p_amp Overall amplitude (Adjusts constrast / sharpness).
+     * @param p_decay Amount of amplitude decay per iteration.
+     * @param p_oct Number of iterations of noise to apply.
+     * @return Perlin noise value.
+     */
     static float Perlin (const float x, const float y,
                          const float p_freq = 1.0f, const float p_amp = 1.0f,
                          const float p_decay = 1.0f, const u32 p_oct = 1);
 
+    /**
+     * Generate a Worley/Cellular noise value for coordinate (x, y).
+     *
+     * @param x x coordinate.
+     * @param y y coordinate.
+     * @param p_freq Frequency of pattern (Higher means smaller cells).
+     * @param p_amp Overall amplitude (Adjusts contrast / sharpness).
+     * @return Worley noise value.
+     */
     static float Worley (const float x, const float y,
                          const float p_freq = 1.0f, const float p_amp = 1.0f);
 };

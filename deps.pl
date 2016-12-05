895f4be2a6af2d0e766e4fa29c92680b635c6260
Stuart Hacking <stuhacking@gmail.com>
2016-12-05 16:27:44 +0000

    Add version/branch specifier to deps.pl

--- deps.pl
+++ deps.pl
@@ -4,26 +4,47 @@ require 5;
 use strict;
 use warnings;
 
-# External dependencies (key will be used in path.)
-my %deps = (
-    soil => 'https://github.com/stuhacking/soil.git',
-    googletest => 'https://github.com/google/googletest.git',
-    rapidjson => 'https://github.com/miloyip/rapidjson.git'
-    );
+# External dependencies (name will form the path so use filesystem friendly names.)
+my @deps_ = (
+    {name => 'soil',       version => 'master', url => 'https://github.com/stuhacking/soil.git'},
+    {name => 'googletest', version => 'master', url => 'https://github.com/google/googletest.git'},
+    {name => 'rapidjson',  version => 'master', url => 'https://github.com/miloyip/rapidjson.git'}
+);
 
 # Directory to place external dependencies.
 my $ext_dir = "External";
 
-sub main () {
-    for my $dep (keys %deps) {
-        my $path = "./$ext_dir/$dep";
-        my $url = $deps{$dep};
+# Check target location for a dependency
+# Dependency should be a hash of {name, url, version}.
+sub check_and_download ($) {
+    my ($dep) = @_;
 
-        if (! -d "$path/.git") {
-            `git clone $url $path`;
-        } else {
-            print STDERR "$dep already downloaded.\n";
-        }
+    die "Unrecognized dependency structure."
+        unless (defined $dep->{name} && defined $dep->{url} && defined $dep->{version});
+
+    my $name = $dep->{name};
+    my $url = $dep->{url};
+    my $version = $dep->{version};
+
+    my $path = "./$ext_dir/$name";
+
+    if (-d "$path/.git") {
+        print STDERR "$path already downloaded.\n";
+        return;
+    }
+
+    if (-d "$path") {
+        print STDERR "$path already exists but does not appear to be a git project.\n";
+        return;
+    }
+
+    # Ok to download.
+    `git clone --depth 1 -b $version $url $path`;
+}
+
+sub main () {
+    for my $dep (@deps_) {
+        check_and_download($dep);
     }
 }
 
